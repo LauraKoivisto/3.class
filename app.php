2087f60f1fb1db4a07d3c9bf41896af83fa11a8e
laukoi <laukoi@greeny.cs.tlu.ee>
2016-02-18 11:03:39 +0200

    add

---
+++ app.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+
+
+
+
+<h2> Message Machine </h2>
+
+
+<form method="get">
+<label for="message">Message:* <label><br>
+<input type="text" name="message"><br>
+<input type="submit" value="save to DB">
+
+
+
+<form>
